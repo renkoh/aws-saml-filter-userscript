9c3f7f5b7c08eaebb1463aa0231e78b2d627d125
Ren Koh <rkoh@seek.com.au>
2020-09-27 23:14:38 +1000

    run prettier

--- aws-saml-filter.user.js
+++ aws-saml-filter.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name        AWS-SSO-filter
+// @name        AWS-SAML-SSO-filter
 // @namespace   signin.aws.amazon.com
-// @description Adds account list filter to AWS SSO signin page
+// @description Adds account list filter to AWS SSO sign-in page
 // @include     https://signin.aws.amazon.com/saml
 // @version     1
 // @grant       none
@@ -11,7 +11,7 @@ window.clearSearch = function () {
     $("#searchbox > input").val("");
     $("#searchbox > input").focus();
     $("#searchbox > input").keyup();
-}
+};
 
 $("#saml_form > p").before(
     '<div id="searchbar">\
@@ -25,7 +25,7 @@ $("#saml_form > p").before(
 <div id="hint">Filter is case-insensitive and ignores dashes \'-\'. Press <b>enter</b> to sign in when your filter returns only one role.</div>'
 );
 
-$("#saml_form > p").hide()
+$("#saml_form > p").hide();
 $("#searchbar").css("display", "flex");
 $("#button_container").css("padding", "24px 0 0 8px");
 $("#searchbox").css("margin", "5px");
@@ -34,9 +34,9 @@ $("#searchbox").css("width", "100%");
 $("#searchbox > input").css("width", "100%");
 $("#searchbox > input").css("padding", "8px 16px");
 $("#searchbox > input").focus();
-$("#hint").css("padding", "0 56px 12px")
-$("#hint").css("margin-top", "-8px")
-$("#hint").css("color", "lightgray")
+$("#hint").css("padding", "0 56px 12px");
+$("#hint").css("margin-top", "-8px");
+$("#hint").css("color", "lightgray");
 
 $("#input_signin_button").before('<div id="filter-error"></div>');
 $("#filter-error").css("padding", "0px 32px 32px");
@@ -44,15 +44,25 @@ $("#filter-error").css("font-size", "16px");
 
 $(document).keypress(function (e) {
     if (e.which == 13) {
-        $("form#saml_form").submit()
+        $("form#saml_form").submit();
     }
 });
 
 $("#searchbox > input").keyup(function (e) {
     if (e.keyCode === 40) {
-        $("fieldset > div.saml-account:visible > div.saml-account > div.saml-role:visible").first().children("input").focus()
-        $("fieldset > div.saml-account:visible > div.saml-account > div.saml-role:visible").first().children("input").prop("checked", true)
-        return
+        $(
+            "fieldset > div.saml-account:visible > div.saml-account > div.saml-role:visible"
+        )
+            .first()
+            .children("input")
+            .focus();
+        $(
+            "fieldset > div.saml-account:visible > div.saml-account > div.saml-role:visible"
+        )
+            .first()
+            .children("input")
+            .prop("checked", true);
+        return;
     }
     if ($(this).val() == "") {
         $("fieldset > div.saml-account").show();
@@ -108,7 +118,7 @@ $("#searchbox > input").keyup(function (e) {
         });
     }
 
-    $('#signin_button').show()
+    $("#signin_button").show();
 
     var visibleAccounts = $("fieldset > div.saml-account:visible");
 
@@ -128,7 +138,7 @@ $("#searchbox > input").keyup(function (e) {
         document.getElementById(
             "filter-error"
         ).innerHTML = `No accounts containing <b>${$(this).val()}</b> found.`;
-        $('#signin_button').hide()
+        $("#signin_button").hide();
     }
 });
 
